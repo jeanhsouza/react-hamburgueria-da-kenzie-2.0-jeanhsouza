f6994df7546ee131dd0464459b38f888296a4a26
Jean Souza <jeanhsouza@gmail.com>
2022-12-17 02:13:07 -0300

    fix/CartBoxMobile

--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const CartBox = styled.div`
 	width: 100%;
-	height: 100%;
+	height: 100vh;
+	max-height: 100%;
 	display: flex;
 	flex-direction: column;
 
