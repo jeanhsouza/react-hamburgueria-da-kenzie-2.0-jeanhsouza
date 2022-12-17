018f09504cf5cdc95d6e063f41bdb85228ce8044
Jean Souza <jeanhsouza@gmail.com>
2022-12-17 11:36:25 -0300

    fix/cartBox OK

--- src/components/Modal/style.ts
+++ src/components/Modal/style.ts
@@ -4,8 +4,8 @@ export const StyledModal = styled.div`
 	position: fixed;
 	right: 0;
 	z-index: 2;
-	width: 100vw;
-	height: 100vh;
+	width: 100%;
+	height: 100%;
 	background-color: rgba(0, 0, 0, 0.5);
 
 	display: flex;
